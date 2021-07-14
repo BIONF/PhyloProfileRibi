cbfcee8ede45dfcee4ad7f6993381e3bd83d4207
trvinh <trvinh@gmail.com>
2021-07-14 10:31:49 +0200

    limit size species plot

--- inst/PhyloProfile/ui.R
+++ inst/PhyloProfile/ui.R
@@ -486,7 +486,7 @@ shinyUI(
             strong(em(
                 paste(
                     "Note: due to the size limit, the font size and dot",
-                    "zooming factor might not affect the plot on SPECIES level!"
+                    "zooming factor may not affect the plot on SPECIES level!"
                 )
             )),
             hr(),
